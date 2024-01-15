81066c28184a6212ab872a43ed4c8decdddb5cc9
kiwon kim <rlarldnjs17@gmail.com>
2024-01-15 16:43:07 +0900

    Feat: 상세페이지 UI 추가

--- app/src/main/java/com/example/anycall/MainActivity.kt
+++ app/src/main/java/com/example/anycall/MainActivity.kt
@@ -2,10 +2,12 @@ package com.example.anycall
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.anycall.databinding.ActivityMainBinding
 
+private val binding by lazy { ActivityMainBinding.inflate(layoutInflater)}
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(binding.root)
     }
 }
