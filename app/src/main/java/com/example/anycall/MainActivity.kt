627268ffa83a3b93af11a61858550de9fc704bb9
kiwon kim <rlarldnjs17@gmail.com>
2024-01-15 17:14:36 +0900

    pull

--- app/src/main/java/com/example/anycall/MainActivity.kt
+++ app/src/main/java/com/example/anycall/MainActivity.kt
@@ -1,13 +1,28 @@
 package com.example.anycall
 
 import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
 import android.os.Bundle
 import com.example.anycall.databinding.ActivityMainBinding
 
 private val binding by lazy { ActivityMainBinding.inflate(layoutInflater) }
+
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
+
+        binding.button1.setOnClickListener {
+            setFragment(ContactsFragment())
+        }
+
+    }
+
+    private fun setFragment(frag: Fragment) {
+        supportFragmentManager.beginTransaction().apply {
+            replace(R.id.frame, frag)
+            setReorderingAllowed(true)
+            addToBackStack("")
+        }.commit()
     }
 }
