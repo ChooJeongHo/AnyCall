5d087365a6b4caebceaaadd28c25910fc7fa87fd
이우진 <dldnwls0115@naver.com>
2024-01-15 16:08:45 +0900

    Init : 프로젝트 생성

---
+++ app/src/main/java/com/example/anycall/Key.kt
@@ -0,0 +1,8 @@
+package com.example.anycall
+
+object Key {
+    val USER_IMAGE = "user_image"
+    val USER_MESSAGE = "user_message"
+    val USER_PHONE = "user_phone"
+    val USER_EMAIL = "user_email"
+}
