477fa0f91b01791c5822ea6532a4705b3616b60b
kiwon kim <rlarldnjs17@gmail.com>
2024-01-16 11:19:34 +0900

    Feat: 상세페이지 Bundle

--- app/src/main/java/com/example/anycall/ContactDetailFragment.kt
+++ app/src/main/java/com/example/anycall/ContactDetailFragment.kt
@@ -5,27 +5,27 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.example.anycall.databinding.FragmentContactDetailBinding
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
+private const val USER_IMAGE = "user_image"
+private const val USER_PHONE = "user_phone"
+private const val USER_MESSAGE = "user_message"
+private const val USER_EMAIL = "user_email"
 
-/**
- * A simple [Fragment] subclass.
- * Use the [ContactDetailFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class ContactDetailFragment : Fragment() {
-    // TODO: Rename and change types of parameters
     private var param1: String? = null
     private var param2: String? = null
+    private var param3: String? = null
+    private var param4: String? = null
+    private val binding by lazy { FragmentContactDetailBinding.inflate(layoutInflater) }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
+            param1 = it.getString(USER_IMAGE)
+            param2 = it.getString(USER_PHONE)
+            param3 = it.getString(USER_MESSAGE)
+            param4 = it.getString(USER_EMAIL)
         }
     }
 
@@ -33,26 +33,19 @@ class ContactDetailFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_contact_detail, container, false)
+        return binding.root
     }
 
     companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment ContactDetailFragment.
-         */
-        // TODO: Rename and change types and number of parameters
+
         @JvmStatic
-        fun newInstance(param1: String, param2: String) =
+        fun newInstance(param1: String, param2: String, param3: String, param4: String) =
             ContactDetailFragment().apply {
                 arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
+                    putString(USER_IMAGE, param1)
+                    putString(USER_PHONE, param2)
+                    putString(USER_MESSAGE, param3)
+                    putString(USER_EMAIL, param4)
                 }
             }
     }
