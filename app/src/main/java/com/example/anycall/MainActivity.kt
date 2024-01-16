526047cc5f966b5a8eb069562315b0e0aa054298
KJ-Hyeon <bark052526@gmail.com>
2024-01-16 18:00:44 +0900

    design: viewPager2 + TabLayout

--- app/src/main/java/com/example/anycall/MainActivity.kt
+++ app/src/main/java/com/example/anycall/MainActivity.kt
@@ -5,28 +5,26 @@ import androidx.fragment.app.Fragment
 import android.os.Bundle
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.anycall.databinding.ActivityMainBinding
+import com.google.android.material.tabs.TabLayoutMediator
 
 class MainActivity : AppCompatActivity(){
     private val binding by lazy { ActivityMainBinding.inflate(layoutInflater) }
+    private val tabTextList = listOf("CONTACT","MY PAGE")
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
 
-        binding.button1.setOnClickListener {
-            setFragment(ContactsFragment())
-        }
-
-        setFragment(ContactsFragment())
+        initViewPager()
     }
 
-    private fun setFragment(frag: Fragment) {
-        supportFragmentManager.beginTransaction().apply {
-            replace(R.id.frame, frag)
-            setReorderingAllowed(true)
-            addToBackStack("")
-        }.commit()
+    private fun initViewPager() {
+        binding.viewpager2.adapter = ViewPagerAdapter(this)
+        TabLayoutMediator(binding.tabLayout, binding.viewpager2) {tab, pos ->
+            tab.text = tabTextList[pos]
+        }.attach()
     }
+
 }
 /**
  * 데이터 넣기, item.xml 구성하기, 롱클릭으로 삭제, 플로팅버튼 추가
