71d2e31231c47cf9573a077d533724cebefa84e2
kiwon kim <rlarldnjs17@gmail.com>
2024-01-16 15:24:07 +0900

    pull

--- app/src/main/java/com/example/anycall/MainActivity.kt
+++ app/src/main/java/com/example/anycall/MainActivity.kt
@@ -1,12 +1,15 @@
 package com.example.anycall
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
 import android.os.Bundle
+import android.view.View
 import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.anycall.MyItem.Companion.dataList
 import com.example.anycall.databinding.ActivityMainBinding
 
-class MainActivity : AppCompatActivity(){
+class MainActivity : AppCompatActivity() {
     private val binding by lazy { ActivityMainBinding.inflate(layoutInflater) }
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -14,7 +17,7 @@ class MainActivity : AppCompatActivity(){
         setContentView(binding.root)
 
 
-        val adapter = MyAdapter(MyItem.dataList)
+        val adapter = MyAdapter(dataList)
         binding.recyclerView.adapter = adapter
         binding.recyclerView.layoutManager = LinearLayoutManager(this)
 
@@ -22,7 +25,14 @@ class MainActivity : AppCompatActivity(){
             setFragment(ContactsFragment())
         }
 
+        adapter.itemClick = object : MyAdapter.ItemClick {
+            override fun onClick(view: View, position: Int) {
+                val fragment = ContactDetailFragment.newInstance(dataList[position].icon, dataList[position].phoneNum, dataList[position].myMessage, dataList[position].email)
+                setFragment(fragment)
+            }
+        }
     }
+
 
     private fun setFragment(frag: Fragment) {
         supportFragmentManager.beginTransaction().apply {
@@ -32,6 +42,7 @@ class MainActivity : AppCompatActivity(){
         }.commit()
     }
 }
+
 /**
  * 데이터 넣기, item.xml 구성하기, 롱클릭으로 삭제, 플로팅버튼 추가
  */
